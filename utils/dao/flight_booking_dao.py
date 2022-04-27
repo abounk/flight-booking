e30525d36dcf8db6a871a597bc333f57fc6f146e
abounk <a_bunk911@hotmail.com>
2022-04-27 16:04:11 +0700

    Add more dao for FlightBooking

--- utils/dao/flight_booking_dao.py
+++ utils/dao/flight_booking_dao.py
@@ -16,7 +16,7 @@ class FlightBookingDao(Dao):
         return booking_list
 
     def get_booking_by_id(self, id: int) -> FlightBooking:
-        """Get a transaction from a specified id.
+        """Get a booking from a specified id.
 
         Args:
             id (int): Booking ID.
@@ -27,6 +27,30 @@ class FlightBookingDao(Dao):
         booking = self.session.query(FlightBooking).filter(
             FlightBooking.id == id).all().pop()
         return booking
+
+    def get_booking_by_user_id(self, user_id: int) -> List[FlightBooking]:
+        """Get a list of flight bookings by user's id.
+
+        Args:
+            user_id (int): id of an user.
+
+        Returns:
+            List[FlightBooking]: All bookings of a specified user.
+        """
+        booking_list = self.__session.query(FlightBooking).filter(
+            FlightBooking.user_id == user_id).all()
+        return booking_list
+
+    def get_booking_by_sorted_date(self) -> List[FlightBooking]:
+        """Get bookings that sorted by onboarding date.
+
+        Returns:
+            List[FlightBooking]: sorted list of FlightBooking obj.
+        """
+        booking_list = self.get_all_booking()
+        booking_by_date_list = sorted(
+            booking_list, key=lambda booking: booking.boarding_date)
+        return booking_by_date_list
 
     def create_booking(self, booking: FlightBooking) -> None:
         """Insert a booking into the database.
